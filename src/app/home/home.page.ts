7b5ab0db0e0f0edb0d18e8a2e5bd2f24c92f14f0
Rich Tillis <rich.tillis@learninga-z.com>
2020-10-22 16:57:32 -0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild, Renderer2 } from "@angular/core";
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  @ViewChild("ioninput") ionInputExample: IonInput;
+  @ViewChild('normalinput', { read: ElementRef }) normalInputExample: ElementRef;
 
-  constructor() {}
+  constructor(private renderer: Renderer2) { }
 
+  ngAfterViewInit() {
+    this.ionInputExample.getInputElement().then(nativeInputInsideIonInput => {
+      this.renderer.addClass(nativeInputInsideIonInput, "aqua-marine");
+      this.renderer.setStyle(nativeInputInsideIonInput,'font-size','x-large');
+      console.log('ion-input: ', nativeInputInsideIonInput);      
+    });
+
+    const ni = this.normalInputExample.nativeElement;
+    this.renderer.addClass(ni, "aqua-marine");
+    this.renderer.setStyle(ni,'font-size','x-large');
+    console.log('ni: ', ni);
+  }
 }
